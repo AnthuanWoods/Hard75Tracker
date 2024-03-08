f548e5fc4f37ab66216459104e303269b4782994
Anthuan <anthuan.e.w@gmail.com>
2024-03-07 20:47:38 -0500

    Added Logging to the CustomEmailController. Added function for creating a category.

--- Hard75API/Controllers/CustomEmailController.cs
+++ Hard75API/Controllers/CustomEmailController.cs
@@ -2,6 +2,7 @@
 using System.Net.Mail;
 using System.Net;
 using Hard75API.Models;
+using Serilog;
 
 namespace Hard75API.Controllers
 {
@@ -14,8 +15,15 @@ namespace Hard75API.Controllers
         [Route("SendEmail")]
         public bool sendEmail([FromBody] CustomEmail ce)
         {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/mylog.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
             try
             {
+                Log.Information("CustomEmail:Creating Email");
                 MailMessage message = new MailMessage();
                 SmtpClient smtp = new SmtpClient();
                 message.From = new System.Net.Mail.MailAddress("anthuan.e.w@gmail.com");
@@ -30,11 +38,13 @@ namespace Hard75API.Controllers
                 smtp.Credentials = new NetworkCredential(message.From.Address, "ybyxeuzppzkwsykd");
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
                 smtp.Send(message);
+                Log.Information("CustomEmail:Email Sent to "+ ce.emailAddress);
                 return true;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
+                Log.Error("CustomEmail:"+ e.ToString());
                 return false;
             }
         }
